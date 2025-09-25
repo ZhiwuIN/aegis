fb782b51481228f4d3cd51836b62b33d9fada8b3
xuesong.lei <228389787@qq.com>
2025-09-25 15:02:21 +0800

    feat: 优化代码

--- src/main/java/com/aegis/config/security/customize/SecurityUser.java
+++ src/main/java/com/aegis/config/security/customize/SecurityUser.java
@@ -45,21 +45,6 @@ public class SecurityUser implements UserDetails {
     }
 
     @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
     public boolean isEnabled() {
         return CommonConstants.NORMAL_STATUS.equals(user.getStatus());
     }
